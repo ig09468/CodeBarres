d09ea23d6e95e68f35fea08f0a0abcfff2ffc3a3
ig0968 <jeremy.meyna@gmail.com>
2019-02-14 14:13:00 +0100

    Ajout de l'énumération

---
+++ code/EAN.h
@@ -0,0 +1,22 @@
+//
+// Created by meynadier on 14/02/19.
+//
+
+#ifndef CODEBARRES_EAN_H
+#define CODEBARRES_EAN_H
+
+/*
+ * Enumération contenant les correspondances des chiffres par rapports aux barres du code
+ */
+namespace ean
+{
+    typedef enum
+    {
+        //TODO A METTRE A JOUR ET AJOUTER LE RESTE DES CORRESPONDANCES
+        ZERO = 3211,
+        ZEROB = 1123
+    }EnumEAN;
+}
+
+
+#endif //CODEBARRES_EAN_H
