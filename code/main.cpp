cb8092f3d3794d7e636067092d9d422b15b39047
Paris Descartes <Descartes@wifi-pro-172-30-41-67.wifi.univ-paris5.fr>
2019-02-20 17:26:17 +0100

    Added possibility to pass image as an argument

--- code/main.cpp
+++ code/main.cpp
@@ -1,9 +1,19 @@
+#include <string>
+#include <sstream>
 #include <iostream>
 #include "test/TestPreProcess.h"
 
 
-int main() {
-    string src = "/home/meynadier/ProjectImg/img/ROTATE.jpg";
+int main(int argc, char *argv[]) {
+    string src;
+
+    if (argc > 1) {
+        stringstream ss;
+        ss << "../img/" << argv[1];
+        src = ss.str();
+    } else {
+        src = "/home/meynadier/ProjectImg/img/ROTATE.jpg";
+    }
     TestPreProcess *test = new TestPreProcess(src, 512);
     test->test();
     return 0;
